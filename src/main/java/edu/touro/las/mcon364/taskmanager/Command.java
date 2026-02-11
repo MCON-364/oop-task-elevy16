11b08179053dbd850d139ca30a9ae1e5f99162ee
Esther <elevy16@student.touro.edu>
2026-02-10 19:56:20 -0500

    Implement step 2: sealed the command hierarchy and discussed how this improves safety

--- src/main/java/edu/touro/las/mcon364/taskmanager/Command.java
+++ src/main/java/edu/touro/las/mcon364/taskmanager/Command.java
@@ -1,3 +1,13 @@
 package edu.touro.las.mcon364.taskmanager;
 
-public interface Command { void execute(); }
+/** A sealed interface improves safety and clarity by allowing you to explicitly control
+ * which classes are allowed to extend or implement them.
+ * Sealed hierarchies mean that the compiler knows all valid subtypes, which improves safety and
+ * enables better support for modern switch expressions.
+ */
+
+public sealed interface Command
+        permits AddTaskCommand, RemoveTaskCommand, UpdateTaskCommand {
+    void execute();
+}
+
